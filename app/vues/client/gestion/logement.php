f62d0569766ab178cdf6c0a7e30d7f5a16e333ef
LucasPerrault <lucas.perrault@hotmail.com>
2018-11-22 07:31:34 +0100

    UPDATE [FRONT] Ajout style (margin etc..) et scrollbar

--- app/vues/client/gestion/logement.php
+++ app/vues/client/gestion/logement.php
@@ -28,7 +28,7 @@
   <div class="card-logement">
     <div class="card-head">
       <ul>
-        <li><h5>16 rue Desnouettes, Paris 75015</h5></li>
+        <li><h5>15 rue trinité, 75013 Paris</h5></li>
         <li>
           <button type="button" name="button" class="button-config-logement" id="button-config" onclick="ouvreParemetresLogement()">
             <img src="<?=ROOT_URL?>/static/image/icon/parameters-logo-lp.png" width="100%" alt="">
@@ -54,7 +54,7 @@
   <div class="card-logement">
     <div class="card-head">
       <ul>
-        <li><h5>16 rue Desnouettes, Paris 75015</h5></li>
+        <li><h5>46 rue balzac, 75006 Paris</h5></li>
         <li>
           <button type="button" name="button" class="button-config-logement" id="button-config" onclick="ouvreParemetresLogement()">
             <img src="<?=ROOT_URL?>/static/image/icon/parameters-logo-lp.png" width="100%" alt="">
@@ -80,33 +80,7 @@
   <div class="card-logement">
     <div class="card-head">
       <ul>
-        <li><h5>16 rue Desnouettes, Paris 75015</h5></li>
-        <li>
-          <button type="button" name="button" class="button-config-logement" id="button-config" onclick="ouvreParemetresLogement()">
-            <img src="<?=ROOT_URL?>/static/image/icon/parameters-logo-lp.png" width="100%" alt="">
-          </button>
-          <nav id="parametres-logement">
-            <ul>
-              <li><a href="#">Supprimer</a></li>
-              <li><a href="#">Modifier</a></li>
-              <li><a href="#">Partager</a></li>
-            </ul>
-          </nav>
-        </li>
-      </ul>
-    </div>
-    <div class="card-body">
-    </div>
-    <div class="card-banniere">
-    </div>
-    <div class="card-footer">
-      <button type="button" name="button" class="button-go-to-piece" onclick="goTo()">Détails</button>
-    </div>
-  </div>
-  <div class="card-logement">
-    <div class="card-head">
-      <ul>
-        <li><h5>16 rue Desnouettes, Paris 75015</h5></li>
+        <li><h5>578 rue des catacombes, 75007 Paris</h5></li>
         <li>
           <button type="button" name="button" class="button-config-logement" id="button-config" onclick="ouvreParemetresLogement()">
             <img src="<?=ROOT_URL?>/static/image/icon/parameters-logo-lp.png" width="100%" alt="">
