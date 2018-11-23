f361182d7b814e78d09c0165916fea6763757695
LucasPerrault <lucas.perrault@hotmail.com>
2018-11-23 23:01:46 +0100

    ADD [FRONT] Création de la nouvelle vue/controleur "détails capteurs" 	- Création du bouton Toggle 	- Création des cards ambiance + programme 	- Commencement de la pop-up

---
+++ app/vues/client/gestion/detailsCapteur.php
@@ -0,0 +1,85 @@
+<div class="container-details-capteur">
+
+  <div class="container-diagramme">
+    <div class="diagramme-circulaire">
+
+    </div>
+    <div class="diagramme-baton">
+
+    </div>
+  </div>
+
+  <div class="container-logo">
+    <h1>Eco'light n°1</h1>
+    <img src="<?=ROOT_URL?>/static/image/entreprise/eco-light.png" width="100%" alt="">
+    <div class="on_off">
+      <label class="toggle-button">
+        <input type="checkbox">
+        <span class="slider round"></span>
+      </label>
+      <span>On / Off</span>
+    </div>
+  </div>
+
+  <div class="container-programme">
+    <div class="ambiance">
+      <h2>Gérer vos ambiances <button type="button" name="button">+</button> </h2>
+      <div class="">
+        <ul>
+          <li>
+            <span>Tamisé</span>
+            <label class="toggle-button">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+          </li>
+          <li>
+            <span>Travail</span>
+            <label class="toggle-button">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+          </li>
+          <li>
+            <span>Illumination Max</span>
+            <label class="toggle-button">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+          </li>
+          <li>
+            <span>Economie</span>
+            <label class="toggle-button">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+    <div class="programme">
+      <h2>Programmer un horaire</h2>
+      <button class="button-ajouter" type="button" name="button">Ajouter</button>
+      <button class="button-visualiser" type="button" name="button">Visualiser</button>
+
+    </div>
+  </div>
+</div>
+
+
+<div class="body">
+  <div class="pop-up">
+    <div class="pop-head">
+      head
+    </div>
+    <div class="pop-text">
+      text
+    </div>
+    <div class="pop-footer">
+      footer
+    </div>
+
+  </div>
+
+</div>
