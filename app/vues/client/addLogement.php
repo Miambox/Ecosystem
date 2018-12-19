ae5f68f4afa9be504276aa099b305a3b3e9972e9
Perrault <lucas.perrault@hotmail.com>
2018-12-19 08:01:22 +0100

    Fix front vue add logement

--- app/vues/client/addLogement.php
+++ app/vues/client/addLogement.php
@@ -1,21 +1,18 @@
 
 <div class="card-addLogement">
   <form class="" action="?Route=client&Ctrl=logement&Vue=addLogement" method="post">
-    <p> Ajout du logement </p>
+    <p> Ajouter votre logement </p>
     <input type="file" name="photo" placeholder="Photo du logement">
     <input type="number" name="numero" placeholder="Numéro">
     <input type="text" name="rue" placeholder="Rue">
     <input type="text" name="ville" placeholder="Ville">
     <input type="number" name="codePostal" placeholder="Code postal">
     <input type="number" name="nbrHabitant" placeholder="Nombre d'habitant">
-    <input type="text" name="pays" placeholder="Pays">
     <input type="number" name="surface" placeholder="Surface du logement">
-    <input type="number" name="anneeConstruction" placeholder="Année de construction">
     <input type="text" name="complementAdresse" placeholder="Complement d'adresse">
+    <input type="number" name="anneeConstruction" placeholder="Année de construction">
     <input type="text" name="diagnotisqueE" placeholder="Diagnostique énergetique">
     <input class="bouton-ajouter" type="submit" name="" value="Ajouter">
-    <!-- <div>
-      <a class"bouton-ajouter" href="<?=ROOT_URL?>?Route=client&Ctrl=piece&Vue=addPiece">Ajout d'une pièce</a>
-    </div> -->
+    <input class="bouton-ajouter-piece" type="submit" name="" value="Ajouter une pièce">
   </form>
 </div>
