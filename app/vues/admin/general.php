929a2a2b6b1bf407ce30ae350b30f405ac70ae99
Perrault <lucas.perrault@hotmail.com>
2019-01-22 21:58:57 +0100

    changer quickly

--- app/vues/admin/general.php
+++ app/vues/admin/general.php
@@ -1,26 +1,32 @@
-<h1> Eco'Admin <h1>
+<div class="general">
+  <h1> Eco'Admin <h1>
 
-  <h2> Comment gérer votre site ? <h2><br>
+    <h2> Comment gérer votre site ? <h2><br>
 
-    <div class="container">
+      <div class="container">
 
-      <div class ="faq">
-      <a href="<?=ROOT_URL?>?Route=admin&amp;Ctrl=faq&amp;Vue=faq" title ="Faq">
-        <p> <img class="crayon" src="<?=ROOT_URL?>static/image/icon/crayon.png">  Éditez votre FAQ </p>
-
+        <div class ="faq">
+          <a href="<?=ROOT_URL?>?Route=admin&amp;Ctrl=faq&amp;Vue=faq">
+          <p>
+            <img class="crayon" src="<?=ROOT_URL?>static/image/icon/crayon.png">
+            Éditez votre FAQ
+          </p>
         </div>
 
         <div class ="mentionsLegales">
-          <a href="<?=ROOT_URL?>?Route=admin&amp;Ctrl=mentionsLegales&amp;Vue=mentionsLegales" title ="Faq" >
-          <p> <img class="crayon" src="<?=ROOT_URL?>static/image/icon/crayon.png">  Éditez vos Mentions Légales</p>
-
-          </div>
-
-          <div class ="cgu">
-             <a href="<?=ROOT_URL?>?Route=admin&amp;Ctrl=general&amp;Vue=cgu">
-            <p> <img src="<?=ROOT_URL?>static/image/icon/crayon.png"> Éditez votre CGU</p>
-          </div>
-
-
-
+          <a href="<?=ROOT_URL?>?Route=admin&amp;Ctrl=mentionsLegales&amp;Vue=mentionsLegales">
+          <p>
+            <img class="crayon" src="<?=ROOT_URL?>static/image/icon/crayon.png">
+            Éditez vos Mentions Légales
+          </p>
         </div>
+
+        <div class ="cgu">
+          <a href="<?=ROOT_URL?>?Route=admin&amp;Ctrl=general&amp;Vue=cgu">
+          <p>
+            <img class="crayon" src="<?=ROOT_URL?>static/image/icon/crayon.png">
+            Éditez votre CGU
+          </p>
+        </div>
+          </div>
+</div>
